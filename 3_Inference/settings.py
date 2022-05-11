dc09605e8b1f7c90f0d96f0bed1fe8c8ed3c9729
Divya Khandelwal <30307748+divyaKh@users.noreply.github.com>
2022-05-10 17:38:00 -0700

    Add settings.py

---
+++ 3_Inference/settings.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# clip length
+L = 15
+winWidth = 1100
+# winWidth = 640
+winHeight = 720
+# winHeight = 480
+
+c = np.random.rand(32, 3) * 255
+sort_max_age = 20
+sort_min_hit = 3
+
